e0901f2138651b07682b6be7b6cec08b222ceddd
nlpxucan <nlpxucan@163.com>
2021-01-12 22:25:45 +0800

    Delete compute_embedding_metrics.py

--- ZRKGC/compute_embedding_metrics.py
+++
@@ -1,5 +0,0 @@
-from nlgeval import compute_metrics
-import sys
-metrics_dict = compute_metrics(hypothesis=sys.argv[1],
-                               references=[sys.argv[2]],no_skipthoughts=True)
-print(metrics_dict)
